cde79e6274529aa459dd1b12b9a76ea0d3303f72
Jesus Torres <j.torres355d@gmail.com>
2020-03-07 21:18:11 -0600

    listad de proveedor

--- lista_proveedor.php
+++ lista_proveedor.php
@@ -1,24 +1,15 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>TODO supply a title</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
         <?php
         include'inc/incluye_bootstrap.php';
         include 'inc/conexion.php';
-        include 'inc/incluye_datatable_head.php';
+       //include 'inc/incluye_datatable_head.php';
         ?>
-    </head>
-    <body>
-        <!--código que incluye el menú responsivo-->
-        <?php include'inc/incluye_menu.php' ?>
+
         <!--termina código que incluye el menú responsivo-->
-        <div class="container">
-            <div class="jumbotron">
+        
                 <?php
                 //Consulta sin parámetros
-                $sel = $con->prepare("SELECT *from marca");
+                $sel = $con->prepare("SELECT *from proveedor");
 
                 /* consulta con parametros
                   $sel = $con->prepare("SELECT *from marca WHERE marca_id<=?");
@@ -28,13 +19,13 @@
 
                 $sel->execute();
                 $res = $sel->get_result();
-                $row = mysqli_num_rows($res);
+                //$row = mysqli_num_rows($res); //numero de registros encontrados
                 ?>
-                Elementos devueltos por la consulta: <?php echo $row ?>
+                
 
                 <select name="select" class="form-control">
                 <?php while ($f = $res->fetch_assoc()) { ?>
-                  <option value="<?php echo $f['marca_id'] ?>"><?php echo $f['marca_nombre'] ?></option> 
+                  <option value="<?php echo $f['proveedor_id'] ?>"><?php echo $f['proveedor_nombre'] ?></option> 
                 
                
 
@@ -44,13 +35,9 @@
                         $sel->close();
                         $con->close();
                         ?>
- </select>
+                        </select>
+
             
        
-        <?php
-        include 'inc/incluye_datatable_pie.php';
-        ?>
-
-    </body>
-</html>
+    
 
